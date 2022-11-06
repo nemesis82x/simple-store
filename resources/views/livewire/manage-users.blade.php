a6b8acd4eb8661bdea240551f0e380c114d5db6d
nemesis <nemesis82@gmail.com>
2022-11-06 18:28:31 +0100

    fa

--- resources/views/livewire/manage-users.blade.php
+++ resources/views/livewire/manage-users.blade.php
@@ -57,7 +57,7 @@
 
                     </div>
 
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 mt-2">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="py-3 px-6">
