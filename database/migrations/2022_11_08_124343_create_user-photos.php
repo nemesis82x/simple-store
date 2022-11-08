155c6da263dfdec6520f1327c96702c463544a55
nemesis82x <nemesis82@gmail.com>
2022-11-08 12:56:18 +0100

    fa uff

---
+++ database/migrations/2022_11_08_124343_create_user-photos.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('user-photos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('path')->nullable();
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user-photos');
+    }
+};
--- resources/views/components/search-table.blade.php
+++ resources/views/components/search-table.blade.php
@@ -19,10 +19,5 @@
     <th scope="col" class="py-3 px-6">
         <span class="sr-only"></span>
     </th>
-    <th scope="col" class=" px-6 text">
-    <input type="submit" value="New"
-           class="bg-green-400 px-6 py-2 rounded-lg font-bold uppercase cursor-pointer shadow
-           onclick="location.href='create'">
-    </th>
 </tr>
 </thead>
