f74a8eafa883be98c2319863ad0dc4942dd7443c
nemesis <nemesis82@gmail.com>
2022-11-08 16:08:57 +0100

    fa

--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -19,20 +19,21 @@
 
                     <!-- Body -->
                     <form>
-                    <div class="flex flex-row space-x-8">
-                        <div>
-                            <img class="w-36 h-36 rounded" src="https://picsum.photos/200" alt="Extra large avatar">
+                    <div class="flex flex-col md:flex-row md:space-x-8">
+                        <div class="w-full md:w-80">
+                            <img class="w-full rounded" src="https://picsum.photos/200" alt="Extra large avatar">
                         </div>
-                        <div class="flex flex-col">
+                        <div class="flex flex-col mt-4 md:mt-0 md:w-80">
+                            <div class="relative z-0 mb-6 w-full group">
+                                <input type="name" name="name" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label for="name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full Name</label>
+                            </div>
+                        </div>
+                        <div class="flex flex-col mt-4 md:mt-0 md:w-80">
                             <div class="relative z-0 mb-6 w-full group">
                                 <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                 <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                             </div>
-                        </div>
-
-                        <div class="flex flex-col">
-                            <input wire:model="pippo" type="text">
-                            <input wire:model="pippo" type="text">
                         </div>
                     </div>
                     </form>
