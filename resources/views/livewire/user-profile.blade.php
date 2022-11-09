4bc68c243a40ad7ab95ff9c9a2c4146f1fbef8d8
nemesis82x <nemesis82@gmail.com>
2022-11-09 11:32:25 +0100

    fa uff

--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -38,11 +38,22 @@
                                 <label for="email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                             </div>
                             <input type="text" wire:model="role">
-                            <input type="text" wire:model="avatar">
-                            <input type="text" wire:model="hero">
-                            <input type="text" wire:model="pic01">
-                            <input type="text" wire:model="pic02">
-                            <input type="text" wire:model="pic03">
+
+                            Avatar
+                            <input type="text" wire:model="name_avatar">
+                            <input type="text" wire:model="path_avatar">
+                            Hero
+                            <input type="text" wire:model="name_hero">
+                            <input type="text" wire:model="path_hero">
+                            Pic01
+                            <input type="text" wire:model="name_pic01">
+                            <input type="text" wire:model="pathpic01">
+                            Pic02
+                            <input type="text" wire:model="name_pic02">
+                            <input type="text" wire:model="path_pic02">
+                            Pic03
+                            <input type="text" wire:model="name_pic03">
+                            <input type="text" wire:model="pathpic03">
                         </div>
                     </div>
                         <div>
